4fe4247631dff73916d15258dba104408ab8318a
simonsays16 <simaopssdias@gmail.com>
2026-02-11 01:00:21 +0000

    pwa menu issues

--- WebApp/sw.js
+++ WebApp/sw.js
@@ -1,5 +1,5 @@
 /* Filename: sw.js */
-const CACHE_NAME = "livetagus-v.b29.11022026"; // Incrementa isto quando fizeres grandes updates
+const CACHE_NAME = "livetagus-v.b33.11022026"; // Incrementa isto quando fizeres grandes updates
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
