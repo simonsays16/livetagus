544f72f4b75bfff42128d5ee221cdc33ef26e6cd
simonsays16 <simaopssdias@gmail.com>
2026-09-06 16:10:32 +0100

    hotfix(css): ficheiro css com versão atualizada para service worker

--- WebApp/sw.js
+++ WebApp/sw.js
@@ -1,5 +1,5 @@
 // --- BEGIN VERSIONS ---
-const GLOBAL_VERSION = "livetagus-v.rc4.06092026";
+const GLOBAL_VERSION = "livetagus-v.rc8.06092026";
 const ASSETS_VERSIONS = {
   "./index.html": "v.rc5.24072026",
   "./index.js": "v.rc3.21062026",
@@ -21,7 +21,7 @@ const ASSETS_VERSIONS = {
   "./privacidade.html": "v.rc1.16082026",
   "./tabs.js": "v.rc1.24052026",
   "./train-scrollbar.js": "v.rc3.22062026",
-  "./output.css": "v.rc7.22062026",
+  "./output.css": "v.rc4.06092026",
   "./menu.js": "v.rc4.02092026",
   "./nav-tools.js": "v.rc9.08062026",
   "./offline.js": "v.rc1.24052026",
